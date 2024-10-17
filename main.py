02e0a9d09f78098022665d41c490cddc7132a83f
joshkeselman <jhkeselman@wpi.edu>
2024-10-17 17:22:32 -0400

    Starting code for demo

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello, World!")
+    tm1 = "" # Teammate 1
+    print("Hello, " + tm1)
+    tm2 = "" # Teammate 2
+    print("Hello, " + tm2)
+
+    team = -1 # Team number
+    print("Hello, Team " + str(team))
+
+if __name__ == "__main__":
+    main()
